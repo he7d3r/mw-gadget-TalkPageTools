145fbc98f35cb083feba25da5e75fb26c40a90ae
Helder <he7d3r@gmail.com>
2012-07-02 19:15:40 +0000

    Restrict the code to #mw-content-text

--- src/TalkPageTools.js
+++ src/TalkPageTools.js
@@ -98,7 +98,7 @@ tpt.formatTalkPage = function () {
 		'div.ongoing-discussion {background-color:#FFF;} ',
 		'.topic {background-color:#EEE;}'
 	].join('\n'));
-	mw.util.$content.find('h2' ).filter(function(i,item){
+	$('#mw-content-text').find('h2' ).filter(function(i,item){
 		var $this = $(item);
 		return !$this.parent().is('#toctitle') && $this.attr('id') !== 'mw-previewheader';
 	}).each(function(){
